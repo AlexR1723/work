809e431c7783fad2a3e245a606bd521d81f76e32
SonGirl <lo.license@yandex.ru>
2019-10-30 20:45:31 +0300

    Фиксированное меню, убрала расстояние input btn

---
+++ static/js/main.js
@@ -0,0 +1,8 @@
+jQuery(window).scroll(
+    function() {
+        var the_top = jQuery(document).scrollTop();
+        if (the_top > 200) {
+            jQuery('#top-a').addClass('fixed-top');      
+        } else {
+            jQuery('#top-a').removeClass('fixed-top');
+        }});
