f1ccae54e55370ce290f9bce8ed2929a127f9936
Anastasiya <romanenko.anastasiya64@gmail.com>
2019-10-30 13:07:23 +0300

    add About page

--- Main/urls.py
+++ Main/urls.py
@@ -18,4 +18,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.Main, name="Main"),
+    url(r'^about/$', views.About, name="About"),
 ]
--- Main/views.py
+++ Main/views.py
@@ -4,3 +4,6 @@ import models
 # Create your views here.
 def Main(request):
     return render(request, 'Main/Main.html', locals())
+
+def About(request):
+    return render(request, 'Main/About.html', locals())
