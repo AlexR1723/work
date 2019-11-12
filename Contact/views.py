a2f61cb25417aa530c21673d410269c514fe6d74
Anastasiya <romanenko.anastasiya64@gmail.com>
2019-11-12 15:36:19 +0300

    работает форма обратной связи

--- Contact/views.py
+++ Contact/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+import json
 from .models import *
+from django.core.mail import send_mail
+
 
 def layout_contact():
     contact=ContactType.objects.all()
@@ -13,3 +17,14 @@ def Contact(request):
     contact = layout_contact()
     link = layout_link()
     return render(request, 'Contact/Contact.html', locals())
+
+def Send(request):
+    name = request.GET.get("name")
+    email=request.GET.get("email")
+    mes=request.GET.get("message")
+    message='Имя пользователя - '+ name + '\n'
+    message=message + 'Email - '+ email + '\n'
+    message=message + mes
+    send_mail('Обратная связь', message, 'romanenko.anastasiya1998@yandex.ua',
+              ['romanenko.anastasiya1998@yandex.ua'], fail_silently=False)
+    return HttpResponse(json.dumps({'data': 'ok'}))
