ef1e0fb6297a9c1989bd18fd562377fbcb4e48c5
Anastasiya <romanenko.anastasiya64@gmail.com>
2019-11-14 15:50:25 +0300

    правки

--- About/views.py
+++ About/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import *
 
+def layout_contact():
+    contact=ContactType.objects.all()
+    return contact
+def layout_link():
+    link=Link.objects.all()
+    return link
 # Create your views here.
 def About(request):
+    contact = layout_contact()
+    link = layout_link()
     return render(request, 'About/About.html', locals())
