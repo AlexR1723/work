6561c44e0a3df82b3cdfa6c9cc704c7b7322fa6a
avdeenko.aleksey@yandex.com <grinya22041982>
2019-12-01 20:40:47 +0300

    вопросы 2

--- Main/admin.py
+++ Main/admin.py
@@ -86,25 +86,7 @@ admin.site.register(BenefitsSafe, BenefitsSafeAdmin)
 
 
 
-class HelpSubcategoryAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in HelpSubcategory._meta.fields]
 
-    class Meta:
-        model = HelpSubcategory
-
-
-admin.site.register(HelpSubcategory, HelpSubcategoryAdmin)
-
-
-
-class HelpCategoryAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in HelpCategory._meta.fields]
-
-    class Meta:
-        model = HelpCategory
-
-
-admin.site.register(HelpCategory, HelpCategoryAdmin)
 
 
 
