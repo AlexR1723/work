575839aad1769291347c5d6e6ecea800ab23a540
Anastasiya <romanenko.anastasiya64@gmail.com>
2019-12-01 17:36:54 +0300

    правки layout

--- Main/views.py
+++ Main/views.py
@@ -22,20 +22,28 @@ def layout_link():
     link=Link.objects.all()
     return link
 
-# def layout_regions():
-#     regions = Region.objects.all()
-#     return regions
-
 def layout_regions_cities(request):
     city = request.session.get('city', 0)
     reg = request.session.get('reg', 0)
     regions = Region.objects.all()
     return city, reg,regions
 
+def layout_name(request):
+    layout = 'layout.html'
+    username=''
+    user = request.session.get('username', 'no')
+    if (user != 'no'):
+        username=AuthUser.objects.all().filter(email=user)[0].first_name
+        user = Users.objects.all().filter(auth_user__email=user)[0]
+        if (user.type.name == "Заказчик"):
+            layout = 'layout_customer.html'
+        else:
+            layout = 'layout_executor.html'
+    return layout,username
+
 # Create your views here.
 def Main(request):
-    layout='layout_customer.html'
-    # layout = 'layout.html'
+    layout,username=layout_name(request)
     contact=layout_contact()
     link=layout_link()
     city,regs,regions=layout_regions_cities(request)
