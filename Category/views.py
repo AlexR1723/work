15ac8f7d4a369ff70b092e06752faa6f3f0148b2
Anastasiya <romanenko.anastasiya64@gmail.com>
2019-12-11 15:49:32 +0300

    постраничная загрузка на подкатегории

--- Category/views.py
+++ Category/views.py
@@ -48,6 +48,9 @@ def Category_item(request,name):
     subs=SubCategory.objects.filter(category_id=category_item.id)
     return render(request, 'Category/Category_item.html', locals())
 
+list_page = []
+sub_count = 0
+
 def sub_category(request,name):
     layout, username, photo = layout_name(request)
     contact = layout_contact()
@@ -55,8 +58,79 @@ def sub_category(request,name):
     city,regs,regions=layout_regions_cities(request)
 
     name=str(name).lower()
-    sub=SubCategory.objects.get(name__icontains=name)
+    sub = SubCategory.objects.get(name__icontains=name)
+    advert_count = UserAdvert.objects.all().filter(subcategory=sub).order_by('-date').count()
     count_user=UserSubcategories.objects.all().filter(subcategories=sub).count()
-    advert=UserAdvert.objects.all().filter(subcategory=sub).order_by('-date')
+    if(advert_count<10):
+        advert=UserAdvert.objects.all().filter(subcategory=sub).order_by('-date')[0:]
+    else:
+        advert=UserAdvert.objects.all().filter(subcategory=sub).order_by('-date')[0:10]
+
+    k = 0
+    while (advert_count > 0):
+        k = k + 1
+        advert_count = advert_count - 10
+    list_page = []
+    page = 1
+    if (k > 6):
+        for i in range(1, 4):
+            list_page.append(i)
+        list_page.append('...')
+        for i in range(k - 2, k + 1):
+            list_page.append(i)
+    else:
+        for i in range(1, k + 1):
+            list_page.append(i)
+    pre = 1
+    next = page + 1
     return render(request, 'Category/Sub_category.html', locals())
 
+def Page_subcategory(request,name,page):
+    layout, username, photo = layout_name(request)
+    contact = layout_contact()
+    link = layout_link()
+    city,regs,regions=layout_regions_cities(request)
+    filter=0
+
+
+    page=int(page)
+    start=page*10-10
+    end=page*10
+    sub=SubCategory.objects.get(name__icontains=name)
+    advert = UserAdvert.objects.all().filter(subcategory=sub).order_by('-date')[start:end]
+    count_user = UserSubcategories.objects.all().filter(subcategories=sub).count()
+
+    advert_count = UserAdvert.objects.all().filter(subcategory=sub).order_by('-date').count()
+
+    k =0
+    while (advert_count > 0):
+        k = k + 1
+        advert_count = advert_count - 10
+    Page = []
+    if k>6:
+        # записать первые 3
+        for i in range(1,4):
+            Page.append(i)
+        # записать середину
+        if page >= 3 and page <= (k - 2):
+            for i in range(page - 1, page + 2):
+                Page.append(i)
+        # записать последние 3
+        for i in range(k - 2, k+1):
+            Page.append(i)
+    else:
+        for i in range(1,k+1):
+            Page.append(i)
+    # убрать повторения
+    Page = list(set(Page))
+    print(Page)
+    list_page = []
+    # добавить '...'
+    for i in range(len(Page) - 1):
+        list_page.append(Page[i])
+        if Page[i + 1] - Page[i] > 1:
+            list_page.append('...')
+    list_page.append(Page[len(Page) - 1])
+    pre = page-1
+    next = page+1
+    return render(request, 'Category/Sub_category.html', locals())
