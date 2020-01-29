0af0fca40c3f842df67f9a2ff0dce6ffa7d7a6dd
Anastasiya <romanenko.anastasiya64@gmail.com>
2020-01-29 14:39:14 +0300

    правка url

--- Main/urls.py
+++ Main/urls.py
@@ -53,7 +53,6 @@ urlpatterns = [
 
     url(r'^dev/$', views.Dev, name="Dev"),
     url(r'^page/$', views.Page, name="Page"),
-    url(r'^notices/$', views.Notices, name="notices"),
     url(r'^profile_unverified/$', views.Profile_unverified, name="profile_unverified"),
     # url(r'^dev/(?P<text>[А-Яа-я\s0-9-()/a-z,]+)/$', views.Dev, name="Dev"),
 ]
